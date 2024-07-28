2c2d321e45fa32f18f7041988bf0fc997d8a52cc
Dana <dana.leenheer@colorado.edu>
2024-07-28 22:50:31 +0000

    updated variable of db route

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World from Dana Leenheer in 3308!'
 
-@app.route(/db_test)
+@app.route('/db_test')
 def testing():
     conn = conn = psycopg2.connect("postgresql://hello_dana_user:2lIRg498XAKrdhUz9MWzGFGZ2ZWKr1Cb@dpg-cqjcbg6ehbks73c9kia0-a/hello_dana")
     conn.close()
