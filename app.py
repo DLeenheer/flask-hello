a31f0c678e8130fc7eb0c33a87b6ddda473dc1fc
Dana <dana.leenheer@colorado.edu>
2024-07-28 23:17:56 +0000

    db_create added

--- app.py
+++ app.py
@@ -14,3 +14,20 @@ def testing():
     conn = psycopg2.connect("postgresql://hello_dana_user:2lIRg498XAKrdhUz9MWzGFGZ2ZWKr1Cb@dpg-cqjcbg6ehbks73c9kia0-a/hello_dana")
     conn.close()
     return 'Database Connection Successful'
+
+@app.route('/db_create')
+def creating():
+    conn = psycopg2.connect("postgresql://hello_dana_user:2lIRg498XAKrdhUz9MWzGFGZ2ZWKr1Cb@dpg-cqjcbg6ehbks73c9kia0-a/hello_dana")
+    cur = conn.cursor()
+    cur.execute.('''
+        CREATE TABLE IF NOT EXISTS Basketball (
+        First varchar(255),
+        Last varchar(255),
+        City varchar(255),
+        Name varchar(255),
+        Number int
+        );
+        ''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Successfully Created"
